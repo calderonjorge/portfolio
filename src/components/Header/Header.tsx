9d016f84eb3164123b1ffbc2c274dee868ada9af
Jorge Calderon <jorge.calderon@globant.com>
2024-01-11 19:24:42 -0600

    scss and hover link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,27 @@
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
-    <header className={`${styles.header}`}>
+    <header className={styles.header}>
       <nav className={styles.navbar}>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem}>About</li>
-          <li className={styles.navItem}>Contact</li>
-        </ul>
+        <div>
+          <p>Jorge Calderon</p>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Lets Connect</p>
+          <button>jorgecalderon@gmail.com</button>
+        </div>
       </nav>
     </header>
   );
