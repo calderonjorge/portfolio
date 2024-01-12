c6d9d2afd8b06b0dcca86959bdd98925f09e4768
Jorge Calderon <jorge.calderon@globant.com>
2024-01-12 17:14:09 -0600

    experience wp

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -11,13 +11,12 @@ export const Experience = () => {
           className={styles["timeline-item"]}
           date-is="NOV 2021 - TODAY"
           letter-is="G"
-          color-is="red"
         >
-          <h3>Web User Interface Developer PROJECT  – Globant</h3>
+          <h3>Web User Interface Developer PROJECT – Globant</h3>
           <ul>
             <li>
-              • Created a frontend project with Next JS, Typescript, Docker, React
-              Hook Form and SASS that manages a complex form (wizard) that
+              • Created a frontend project with Next JS, Typescript, Docker,
+              React Hook Form and SASS that manages a complex form (wizard) that
               captures the information of a resume.
             </li>
             <li>
@@ -33,39 +32,45 @@ export const Experience = () => {
         </article>
 
         <article
-         className={styles["timeline-item"]} 
-         date-is="MAY 2021 - NOV 2021"
-         letter-is="U"
-         color-is="red"
-         >
+          className={styles["timeline-item"]}
+          date-is="MAY 2021 - NOV 2021"
+          letter-is="U"
+        >
           <h3>Frontend Developer – UPAX</h3>
           <ul>
             <li>
-            • Developed multiple websites implemented throughout Mexico using modern technologies as NextJS, Redux, Material UI, and Typescript, exceeding the access of new users with respect to the old ones by 23%
+              • Developed multiple websites implemented throughout Mexico using
+              modern technologies as NextJS, Redux, Material UI, and Typescript,
+              exceeding the access of new users with respect to the old ones by
+              23%
             </li>
             <li>
-            • Participated in the design and fulfillment of functional requirements.
+              • Participated in the design and fulfillment of functional
+              requirements.
             </li>
             <li>
-            • Used geospatial technology with the Google Maps API (geolocation and visualization of coordinates).
+              • Used geospatial technology with the Google Maps API (geolocation
+              and visualization of coordinates).
             </li>
           </ul>
         </article>
 
-        <article 
-        className={styles["timeline-item"]} 
-        date-is="20-07-1990"
-        letter-is="I"
+        <article
+          className={styles["timeline-item"]}
+          date-is="MAY 2019 - AUG 2021"
+          letter-is="I"
         >
-          <h3>I&apos;m ::last-child so my border fades ^__^</h3>
-          <p>
-            I&apos;m speaking with myself, number one, because I have a very
-            good brain and I&apos;ve said a lot of things. I write the best
-            placeholder text, and I&apos;m the biggest developer on the web by
-            far... While that&apos;s mock-ups and this is politics, are they
-            really so different? I think the only card she has is the Lorem
-            card.
-          </p>
+          <h3>Web Developer – Instituto Mexicano de Tecnología del Agua</h3>
+          <ul>
+            <li>
+              • Full-stack developer, under SISUAR project. SISUAR is a
+              geospatial query system about the uses of irrigation water in
+              agriculture in Mexico.
+            </li>
+            <li>• Designed and implemented and managed a MySQL database.</li>
+            <li>• API creation and consumption.</li>
+            <li>• Apache HTTP server configuration.</li>
+          </ul>
         </article>
       </div>
     </Container>
