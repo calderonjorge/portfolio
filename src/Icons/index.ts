9b13c6d93880853b59983777b8660d387d999f3e
Jorge <jorgecalderon.dev@gmail.com>
2024-01-13 16:39:39 -0600

    icons

--- src/Icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { Copy } from './Copy'
-export { CopyCheck } from './CopyCheck'
-export { LinkedIn } from './LinkedIn'
-export { Instagram } from './Instagram'
-export { Github } from './Github'
