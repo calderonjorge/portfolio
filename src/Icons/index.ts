ab70b11fc3d4775063a65b36cf7358e1b2aa7e72
Jorge <jorgecalderon.dev@gmail.com>
2024-01-13 16:42:07 -0600

    Revert "icons"
    This reverts commit 9b13c6d93880853b59983777b8660d387d999f3e.

---
+++ src/Icons/index.ts
@@ -0,0 +1,5 @@
+export { Copy } from './Copy'
+export { CopyCheck } from './CopyCheck'
+export { LinkedIn } from './LinkedIn'
+export { Instagram } from './Instagram'
+export { Github } from './Github'
