d7e25cf9dd643c36e2f8e6d5fdc5eb87a5159ea3
Jorge Calderon <jorge.calderon@globant.com>
2024-01-15 09:33:16 -0600

    jeje

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -4,13 +4,21 @@ import styles from './LanguagePicker.module.scss'
 export const LanguagePicker = () => {
   return (
     <div className={styles.language}>
-      <select className={styles.languageSelect} onChange={() => { console.error('changed') }}>
-        <option value="en" selected>English</option>
+      <span>hello worl jijij</span>
+      <select
+        className={styles.languageSelect}
+        onChange={() => {
+          console.error('changed')
+        }}
+      >
+        <option value="en" selected>
+          English
+        </option>
         <option value="es">Spanish</option>
       </select>
       <div className={styles.languageIcon}>
         <Globe className={styles.globe} />
-        <ChevronDown className={styles.chevron}/>
+        <ChevronDown className={styles.chevron} />
       </div>
     </div>
   )
