b9af898721a9fec42662d9b7fc42cfea3faf3f91
Jorge Calderon <jorge.calderon@globant.com>
2024-01-15 14:02:03 -0600

    hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,15 +6,12 @@ export const Hero = () => {
   const { t } = useTranslate()
   return (
     <section className={styles.hero} id={t('hero.id')}>
-      <h1>
-        Senior <br />
-        web developer
-      </h1>
+      <h1>{t('hero.job_title')}</h1>
       <div className={styles.currentJob}>
         <p>
-          Creating amazing user interfaces at{' '}
-          <Link href="https://www.globant.com/" target="_blank" rel="noreferrer">
-            @Globant
+          {t('hero.current_job')}{' '}
+          <Link href={t('hero.current_company_href')} target="_blank" rel="noreferrer">
+            @{t('hero.current_company_name')}
           </Link>
         </p>
         <p>Oct 2021 - Present</p>
