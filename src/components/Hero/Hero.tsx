b048fb72c7f396948f7c626c6cb789927d475288
Jorge Calderon <jorge.calderon@globant.com>
2024-01-15 15:36:00 -0600

    hero-text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
-import styles from './Hero.module.scss'
 import { useTranslate } from '@/hooks'
+import styles from './Hero.module.scss'
 
 export const Hero = () => {
   const { t } = useTranslate()
   return (
     <section className={styles.hero} id={t('hero.id')}>
       <h1>{t('hero.job_title')}</h1>
+
       <div className={styles.currentJob}>
         <p>
           {t('hero.current_job')}{' '}
