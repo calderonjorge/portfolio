a98b1be4ee2b6e0b96e83fba84f26b90887c513c
Jorge Calderon <jorge.calderon@globant.com>
2024-01-16 21:58:23 -0600

    mobile header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { Container, Link, AwesomeLink } from '@/components'
+import { Menu } from '@/icons'
 import { useTranslate } from '@/hooks'
 import styles from './Header.module.scss'
+import Image from 'next/image'
 
 export const Header = () => {
   const { t } = useTranslate()
@@ -9,7 +11,8 @@ export const Header = () => {
     <header className={styles.header}>
       <Container component="nav" className={styles.navbar}>
         <div className={styles.logo}>
-          <p>Jorge Calderon</p>
+          {/* TODO: create a logo */}
+          <Image src={'https://flowbite.com/docs/images/logo.svg'} height={32} width={32} alt="" />
         </div>
         <ul className={styles.links}>
           <li>
@@ -26,6 +29,11 @@ export const Header = () => {
           <AwesomeLink href={t('header.links.get_in_touch.href')}>
             {t('header.links.get_in_touch.value')}
           </AwesomeLink>
+        </div>
+        <div className={styles.menu}>
+          <button>
+            <Menu />
+          </button>
         </div>
       </Container>
     </header>
---
+++ src/icons/Menu.tsx
@@ -0,0 +1,20 @@
+export const Menu = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 6l16 0" />
+      <path d="M4 12l16 0" />
+      <path d="M4 18l16 0" />
+    </svg>
+  )
+}
