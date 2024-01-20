d7609d0351b696e50e8fc69fc1d787a6bcb11a4e
calderonjorge <jorgecalderaperalta@gmail.com>
2024-01-20 13:43:33 -0600

    build fixed

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 
 export const useClickOutside = (ref: any, callback: () => void) => {
-  const handleClick = e => {
+  const handleClick = (e: globalThis.MouseEvent) => {
     if (ref.current && !ref.current.contains(e.target)) {
       callback()
     }
