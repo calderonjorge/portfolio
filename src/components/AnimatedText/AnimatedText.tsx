628f2e99999abdbea3fe50ad26b62c5f9a4f5537
Jorge Calderon <jorge.calderon@globant.com>
2024-01-28 00:23:54 -0600

    safari support

--- src/components/AnimatedText/AnimatedText.tsx
+++ src/components/AnimatedText/AnimatedText.tsx
@@ -15,10 +15,39 @@ export const AnimatedText: FC<PropsWithChildren<Props>> = ({ children }) => {
       !CSS.supports('animation-timeline: scroll()') &&
       matchMedia('(prefers-reduced-motion: no-preference)')
     ) {
+      gsap.registerPlugin(ScrollTrigger)
+
+      const scrub = 0.5
+      const trigger = '#animation'
+
+      gsap.set('p > span', {
+        '--progress': 0,
+        'backgroundPositionX':
+          'calc(-200vmax + (var(--progress) * 200vmax)), calc(-200vmax + (var(--progress) * 200vmax)), 0',
+        'color': 'transparent',
+      })
+      gsap.to('p > span', {
+        '--progress': 1,
+        'scrollTrigger': {
+          trigger,
+          scrub,
+          start: 'top top',
+          end: 'top top-=75%',
+        },
+      })
+      gsap.to('p > span', {
+        color: '#cacaca',
+        scrollTrigger: {
+          trigger,
+          scrub,
+          start: 'top top-=75%',
+          end: 'bottom bottom',
+        },
+      })
     }
   }, [])
   return (
-    <Container className={styles.section}>
+    <Container className={styles.section} id="animation">
       <div className={styles.container}>
         <p>
           <span>{children}</span>
