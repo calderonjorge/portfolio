465cd9a72680a871d13ecbca1733405c8c21138e
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-09 23:56:18 -0600

    db list

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,13 @@
 import Database from 'better-sqlite3'
 
+const fs = require('fs')
+
+// Obtener la ruta del directorio actual
+const directorioActual = './src/db'
+
+// Listar los archivos en el directorio actual
+fs.readdirSync(directorioActual).forEach(archivo => {
+  console.error(archivo)
+})
+
 export const db = new Database('./src/db/loans.db')
